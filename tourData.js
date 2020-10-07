ee17bc31d1391e9731bb87c0f0314403a12e2514
kostars <66394914+kostars@users.noreply.github.com>
2020-10-07 20:24:03 +0100

    Add files via upload

---
+++ tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+    {
+      id: 1,
+      city: "new york",
+      img: "./img/Anewyork.jpeg",
+      name: "new york bridge tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 2,
+      city: "paris",
+      img: "./img/Aparis.jpeg",
+      name: "paris lights tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 3,
+      city: "london",
+      img: "./img/Alondon.jpeg",
+      name: "london royal palace tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    },
+    {
+      id: 4,
+      city: "tokyo",
+      img: "./img/Atokyo.jpeg",
+      name: "tokyo sushi tour",
+      info:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+    }
+  ];
