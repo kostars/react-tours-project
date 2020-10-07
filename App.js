893baac77ea3ee37024496b68524814ad274805e
kostars <66394914+kostars@users.noreply.github.com>
2020-10-07 20:24:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <TourList />
+        
+      </div>
+    )
+  }
+}
+
+export default App
